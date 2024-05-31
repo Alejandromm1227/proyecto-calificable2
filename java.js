d7eac63790fc2296c3d989b282be019bb9de17bb
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-05-31 15:48:12 -0500

    proyecto terminado

--- java.js
+++ java.js
@@ -2,3 +2,21 @@ function mostrarMensaje() {
     var mensaje = "¡Sigue adelante, estás haciendo un gran trabajo!";
     document.getElementById("mensaje").innerHTML = mensaje;
 }
+
+const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit", validarFormulario)
+
+function validarFormulario(e){
+    e.preventDefault();
+    const nombre = document.querySelector("#nombre").value
+    const apellido = document.querySelector("#apellido").value
+    const correo = document.querySelector("#correo").value
+    const mensaje = document.querySelector("#mensaje").value
+
+
+    console.log("Nomnbre: ", nombre);
+    console.log("Apellido: ", apellido);
+    console.log("Correo: ", correo);
+    console.log("mensaje: ", mensaje);
+}
